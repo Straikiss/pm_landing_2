fe690f8beb90a2a65348ad16eb26b85842d29504
Straikiss <straikiss.web@gmail.com>
2023-02-26 15:48:14 +0300

    2.3
    Easy animate on 1st scroll all section

--- js/main.js
+++ js/main.js
@@ -66,3 +66,18 @@ if(navigator.language.slice(0,2) == "es")
   changeLanguage("es");
 else
   changeLanguage("en");
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if(change.isIntersecting) 
+      change.target.classList.add('animate');
+  });
+}
+  
+let options = {threshold: [0.5]};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('section');
+  
+for(let elm of elements)
+  observer.observe(elm);
