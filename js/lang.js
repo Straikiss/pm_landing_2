02c63adf2bd9615c647f9395b51631960f959e80
Straikiss <straikiss.web@gmail.com>
2023-03-02 17:07:16 +0300

    main
    Update main

---
+++ js/lang.js
@@ -0,0 +1,146 @@
+let lang = {
+  "title": {
+    "en": "System to buy promotion from Instagram and TikTok users - PostMarket",
+    "es": "Sistema para comprar anuncios y usuarios en Instagram y Tik Tok - PostMarket"
+  },
+  "header-mission": {
+    "en": "Our mission",
+    "es": "Nuestra misión"
+  },
+  "header-offers": {
+    "en": "Get offers",
+    "es": "Recibe ofertas"
+  },
+  "header-ads": {
+    "en": "Buying ads",
+    "es": "Compra de anuncios"
+  },
+  "header-learn": {
+    "en": "Learn",
+    "es": "Aprender"
+  },
+  "header-platform": {
+    "en": "Platform work",
+    "es": "Trabajo de plataforma"
+  },
+  "header-blog": {
+    "en": "Blog",
+    "es": "Blog"
+  },
+  "header-clients": {
+    "en": "Clients",
+    "es": "Clientes"
+  },
+  "mission-title": {
+    "en": "Your <span class='mission__block__title__text'>profit 🤑</span> in social media 💫",
+    "es": "Tu <span class='mission__block__title__text'>beneficio 🤑</span> en las redes sociales 💫"
+  },
+  "mission-text": {
+    "en": "+ learn and promote 🔥 your account with us 🤍",
+    "es": "+ aprende y promociona 🔥 tu cuenta con nosotros 🤍"
+  },
+  "mission-qr": {
+    "en": "Point your smartphone camera and download the app",
+    "es": "Apunta la cámara de tu smartphone y descarga la aplicación"
+  },
+  "mission-title-2": {
+    "en": "Our mission",
+    "es": "Nuestra misión"
+  },
+  "mission-text-2": {
+    "en": "В чем наша миссия по отношениию к блогеру в чем удобство использования с кем сотруд. приложение Мы хотим чтобы вы делали уверенно перевые шаги .в чем наша миссия по отношениию к блогеру в чем удобство использования с кем сотруд. приложение Мы хотим чтобы вы делалали уверенно перевые шаги .в чем наша миссия по отношениию к блогеру в чем удобство использования с кем сотруд. приложение Мы хотим чтобы вы делалали уверенно перевые шаги.",
+    "es": "Cuál es nuestra misión en relación al blogger, cuál es la facilidad de uso con quien trabajamos. app Queremos que des los primeros pasos con confianza cuál es nuestra misión en relación con el blogger cuál es la facilidad de uso con quien trabajamos. aplicación Queremos que des el primer paso con confianza cuál es nuestra misión en relación con el blogger cuál es la facilidad de uso con quien trabajamos. aplicación Queremos que des el primer paso con confianza."
+  },
+  "platform-faq-title-1": {
+    "en": "How to earn with PostMarket?",
+    "es": "¿Cómo Ganar Dinero en PostMarket?"
+  },
+  "platform_block_text-1": {
+    "en": "It’s easy – just go to My applications chapter and select the applications seeking promotion that fit you and your target audience best! You may judge based on your account focus. Want to gain maximum response from the audience and boost your reward? Use Improvise mode and make the most of it all! Create a series of posts to keep your audience warm! Invent interesting scenarios or you can use some of ours!",
+    "es": "Es muy fácil. Ve a la sección de 'mis aplicaciones' y elige la propuesta publicitaria que sea más adecuada para ti, como para tu público meta y que vaya con la temática de tu cuenta. ¿Quieres recibir la mayor cantidad posible de clics por parte de tu público y aumentar tus recompensas? Usa el régimen de “improvisación” y ¡Recibe los mejores resultados! Crea una serie de publicaciones para emocionar a tu público. Inventa un guión interesante o elige los nuestros. "
+  },
+  "platform_block_text-2": {
+    "en": "Your ad is good but the audience is still not really responding? You don’t have to leap into despair – there is always room to grow! This is where Postmarket can help you promote your account bringing it up to the next level, and have your achievements growing constantly.",
+    "es": "¿Hiciste un gran anuncio, pero tu público reaccionó poco a él? No te preocupes ¡Siempre tendrás espacio donde crecer! Justo por eso PostMarket te puede ayudar a llevar a tu cuenta a un nuevo nivel y mejorar tus resultados de manera gradual."
+  },
+  "platform-faq-title-2": {
+    "en": "How many accounts and what social media can be used?",
+    "es": "¿Cuántas cuentas puedo usar y de qué redes sociales?"
+  },
+  "platform_block_text-3": {
+    "en": "Right now, you can connect your Instagram or TikTok account. You can add up to 10 accounts from different social media! Have more than one account? Great! You can take up promotion running it from several blogs and double your reward.",
+    "es": "Actualmente puedes conectar tu cuenta a Instagram y TikTok. Puedes agregar hasta 10 cuentas de diferentes redes sociales. ¿Tienes varias cuentas? ¡Excelente! Puedes realizar la publicidad inmediatamente con varios blogs y recibir tus recompensas.Actualmente puedes conectar tu cuenta a Instagram y TikTok. Puedes agregar hasta 10 cuentas de diferentes redes sociales. ¿Tienes varias cuentas? ¡Excelente! Puedes realizar la publicidad inmediatamente con varios blogs y recibir tus recompensas."
+  },
+  "platform-faq-title-3": {
+    "en": "How to boost your account through PostMarket?",
+    "es": "¿Cómo mejorar tu cuenta a través de PostMarket?"
+  },
+  "platform_block_text-4": {
+    "en": "PostMarket offers a whole range of ways to promote your account. You can go to Promotion purchase chapter, launch a promo advertising your Instagram or TikTok account and get true and motivated followers. You can go to Account promotion chapter and use the service for individual work where our experts will take care of making your account better – all the metrics of it. Still have questions? Write to us (see the blank form below).",
+    "es": "PostMarket cuenta con una amplia gama de posibilidades para promover tu cuenta. Puedes ir a la sección de “compra de promoción” y lanzar anuncios para tu cuenta en Instagram o TikTok, para recibir suscriptores reales y activos. Puedes ir a la sección de “Promoción de cuenta” y utilizar los servicios individuales de nuestros expertos para mejorar las métricas de tu cuenta. Si aún tienes preguntas, escríbenos. El formulario se encuentra abajo. PostMarket cuenta con una amplia gama de posibilidades para promover tu cuenta. Puedes ir a la sección de “compra de promoción” y lanzar anuncios para tu cuenta en Instagram o TikTok, para recibir suscriptores reales y activos. Puedes ir a la sección de “Promoción de cuenta” y utilizar los servicios individuales de nuestros expertos para mejorar las métricas de tu cuenta. Si aún tienes preguntas, escríbenos. El formulario se encuentra abajo."
+  },
+  "platform-faq-title-4": {
+    "en": "Will my accounts remain safe if I work with PostMarket?",
+    "es": "¿Qué tan seguro es para mi cuenta trabajar con PostMarket?"
+  },
+  "platform_block_text-5": {
+    "en": "Work with PostMarket is totally safe for any account you connect. PostMarket requires no signing up – just let us know your nick and we will have a look at your account, very much like another follower visiting your page.",
+    "es": "Trabajar con PostMarket es totalmente seguro para cualquier cuenta que vayas a utilizar. PostMarket no requiere autorizaciones de tu parte. Tu necesitas mostrarnos solamente tu nombre de cuenta y revisaremos tu perfil, tan solo como si nosotros fuéramos uno de tus seguidores. Trabajar con PostMarket es totalmente seguro para cualquier cuenta que vayas a utilizar. PostMarket no requiere autorizaciones de tu parte. Tu necesitas mostrarnos solamente tu nombre de cuenta y revisaremos tu perfil, tan solo como si nosotros fuéramos uno de tus seguidores."
+  },
+  "platform-faq-title-5": {
+    "en": "How to attract new followers to my Instagram?",
+    "es": "¿Cómo atraer seguidores reales a tu cuenta de Instagram?"
+  },
+  "platform_block_text-6": {
+    "en": "PostMarket offers a unique way to attract only true and real followers who may find your profile interesting. It’s simple! You create a promo campaign in Promotion purchase chapter, then choose Cost per follower and create a campaign. While creating it, you will indicate all the criteria required, then select the cost, how much you are willing to pay per 1 follower, and then drop some funds to your budget.Further on, bloggers who find your offer good enough will start promoting you, while getting paid only for the followers who used to be their followers, then started following you and never unfollowed for 10+ days!",
+    "es": "En PostMarket existe la posibilidad única de atraer sólo a seguidores reales que se interesen en tu perfil. ¡Es muy sencillo! Creas una campaña de publicidad en la sección de “Compra de promoción”, luego eliges “Costo por seguidor” y creas la campaña publicitaria. Durante este proceso tú eliges los criterios deseados, cuanto deseas pagar por seguidor y abonas tu presupuesto. Luego otros blogueros que consideren que tu propuesta va con ellos comenzarán a hacerte publicidad y a recibir dinero sólo por los seguidores que, estando suscritos a ellos, se suscriban a ti por más de 10 días. En PostMarket existe la posibilidad única de atraer sólo a seguidores reales que se interesen en tu perfil. ¡Es muy sencillo! Creas una campaña de publicidad en la sección de “Compra de promoción”, luego eliges “Costo por seguidor” y creas la campaña publicitaria. Durante este proceso tú eliges los criterios deseados, cuanto deseas pagar por seguidor y abonas tu presupuesto. Luego otros blogueros que consideren que tu propuesta va con ellos comenzarán a hacerte publicidad y a recibir dinero sólo por los seguidores que, estando suscritos a ellos, se suscriban a ti por más de 10 días."
+  },
+  "platform-faq-title-6": {
+    "en": "What type of promotional campaign can I take up?",
+    "es": "¿Qué tipos de campañas publicitarias puedo elegir para trabajar?"
+  },
+  "platform_block_text-7": {
+    "en": "The system offers three types of campaigns for our users to choose from – cost per mille (CPM), cost per action (CPA) and cost per follower (CPF).",
+    "es": "En el sistema existen tres tipos diferentes de campañas accesibles a nuestros usuarios.  Campañas con pago por compartir publicidad, con pago por seguidores y con pago por acciones.En el sistema existen tres tipos diferentes de campañas accesibles a nuestros usuarios.  Campañas con pago por compartir publicidad, con pago por seguidores y con pago por acciones."
+  },
+  "platform-faq-title-7": {
+    "en": "Can I start promotion through the app?",
+    "es": "¿Puedo iniciar la promoción a través de la aplicación?"
+  },
+  "platform_block_text-8": {
+    "en": "Of course! To launch a promo campaign, go to Promotion Purchase chapter and select the type of campaign that fits you best. You can also use this website to launch your campaign. <a class='platform__block__text__link' href='https://postmarket.pro/Advertisers' target='_blank'>postmarket.pro/advertisers</a>",
+    "es": "¡Claro! Para lanzar una campaña publicitaria es necesario ir a la sección de “Compra de promoción” y elegir la campaña publicitaria que se ajuste mejor a tus necesidades. También puedes usar el sitio y lanzar tu campaña ahí. <a class='platform__block__text__link' href='https://postmarket.pro/Advertisers' target='_blank'>postmarket.pro/advertisers</a>"
+  },
+  "platform-faq-title-8": {
+    "en": "How can I withdraw?",
+    "es": "¿Qué tan rápido puedo retirar estos fondos?"
+  },
+  "platform_block_text-9": {
+    "en": "In case you have got funds on your balance, you can withdraw them immediately except for money earned for followers (this will take a 10-day waiting period to see who unfollows).",
+    "es": "Si tienes dinero en tu balance, lo puedes retirar de inmediato, a excepción de las ganancias obtenidas por campañas para obtención de seguidores, ya que se realiza una revisión a los diez días para asegurar que no hayan dejado de suscribirse al cliente."
+  },
+  "platform-faq-title-9": {
+    "en": "How do I get promotional requests CPM?",
+    "es": "¿Cómo recibir solicitudes publicitarias con pago sólo por publicación? "
+  },
+  "platform_block_text-10": {
+    "en": "Requests for promotion on the cost-per-mille (CPM) basis may be available to any type of Postmarket users. However, the higher the blogger’s status (link to it), the more of such requests you will get from Postmarket. Some brands may invite you to be their ambassador in case they like the way you advertise something in a different format!",
+    "es": "Las solicitudes publicitarias con pago sólo por publicación están accesibles para cualquier tipo de usuario de PostMarket. Pero mientras más alto sea tu estatus de bloguer, más solicitudes con pago por publicación recibirás de PostMarket. ¡Algunas marcas pueden elegirte como su embajador, si les agrada tus publicaciones en diferentes formatos! Las solicitudes publicitarias con pago sólo por publicación están accesibles para cualquier tipo de usuario de PostMarket. Pero mientras más alto sea tu estatus de bloguer, más solicitudes con pago por publicación recibirás de PostMarket. ¡Algunas marcas pueden elegirte como su embajador, si les agrada tus publicaciones en diferentes formatos!"
+  },
+  "platform-faq-title-10": {
+    "en": "What is partnership?",
+    "es": "¿Qué es el programa de socios?"
+  },
+  "platform_block_text-11": {
+    "en": "Come to become a PostMarket partner! Promote our partnership link and get 10% from the income of the involved users, and 10% from the advertisers’ expenditures.",
+    "es": "¡Conviértete en socio de PostMarket! Promueve nuestro enlace de socio y gana 10% de los ingresos de los ingresos de los usuarios obtenidos por redes sociales y el 10% de los gastos de los anunciantes. ¡Conviértete en socio de PostMarket! Promueve nuestro enlace de socio y gana 10% de los ingresos de los ingresos de los usuarios obtenidos por redes sociales y el 10% de los gastos de los anunciantes."
+  },
+  "platform-faq-title-11": {
+    "en": "What is the blogger’s status and what does it mean?",
+    "es": "¿Qué es el Estatus de Bloguer y qué es lo que hace?"
+  },
+  "platform_block_text-12": {
+    "en": "The blogger’s status determines the number and the quality of the requests you see in My Applications chapter. The better you complete applications seeking promotion, and the more of them you take up, the higher is your status!",
+    "es": "El estatus de bloguer determina la cantidad y calidad de propuestas que ves en la sección de “mis aplicaciones”. Mientras mejor realices las solicitudes publicitarias, mientras más trabajes en ellas, más alto será tu estatus. Más información sobre estatus aquí."
+  },
+}
