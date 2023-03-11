9983f71a7719201a7eb18d55eca9984a6538b0f3
Straikiss <straikiss.web@gmail.com>
2023-03-11 17:51:11 +0300

    mian

--- js/main.js
+++ js/main.js
@@ -139,3 +139,8 @@ function CloseModal(ModalName){
 
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
